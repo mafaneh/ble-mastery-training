9fb7ca97aa6171c5684029bce7786a5fe8d6128b
Mohammad Afaneh <mafaneh@gmail.com>
2024-08-06 23:03:40 -0400

    Upgrade advertising examples to 4.4.3 SDK

--- silabs/1_advertising/bt-adv-data/GNU ARM v12.2.1 - Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /Users/mafaneh/SimplicityStudio/SDKs/gecko_sdk_3//protocol/bluetooth/bgcommon/lib/libbgcommon_efr32xg22_gcc_release.a /Users/mafaneh/SimplicityStudio/SDKs/gecko_sdk_3//protocol/bluetooth/bgstack/ll/lib/libbluetooth_controller_efr32xg22_gcc_release.a /Users/mafaneh/SimplicityStudio/SDKs/gecko_sdk_3//protocol/bluetooth/lib/libbluetooth_host_efr32xg22_gcc_release.a /Users/mafaneh/SimplicityStudio/SDKs/gecko_sdk_3//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a /Users/mafaneh/SimplicityStudio/SDKs/gecko_sdk_3//platform/radio/rail_lib/autogen/librail_release/librail_config_bgm220pc22hna_gcc.a /Users/mafaneh/SimplicityStudio/SDKs/gecko_sdk_3//platform/radio/rail_lib/autogen/librail_release/librail_module_efr32xg22_gcc_release.a
-
-LIBS := -lgcc -lc -lm -lnosys
-
