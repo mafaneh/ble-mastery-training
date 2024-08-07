cb20d296e19dac9914b02ec36e1bc2478de837a4
Mohammad Afaneh <mafaneh@gmail.com>
2024-08-07 14:10:37 -0400

    Remove old Chained Extended Adv example

--- silabs/9_advanced/bt-ext-adv-chained/GNU ARM v12.2.1 - Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /Users/mafaneh/ble-mastery-training/silabs/9_advanced/bt-ext-adv-chained/simplicity_sdk_2024.6.1/protocol/bluetooth/bgcommon/lib/build/gcc/cortex-m33/bgcommon/release/libbgcommon.a /Users/mafaneh/ble-mastery-training/silabs/9_advanced/bt-ext-adv-chained/simplicity_sdk_2024.6.1/protocol/bluetooth/bgstack/ll/lib/libbluetooth_controller_efr32xg22_gcc_release.a /Users/mafaneh/ble-mastery-training/silabs/9_advanced/bt-ext-adv-chained/simplicity_sdk_2024.6.1/protocol/bluetooth/build/gcc/cortex-m33/bt_host/hal/release/libbt_hal_series2.a /Users/mafaneh/ble-mastery-training/silabs/9_advanced/bt-ext-adv-chained/simplicity_sdk_2024.6.1/protocol/bluetooth/build/gcc/cortex-m33/bt_host/release/libbt_host.a /Users/mafaneh/ble-mastery-training/silabs/9_advanced/bt-ext-adv-chained/simplicity_sdk_2024.6.1/platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a /Users/mafaneh/ble-mastery-training/silabs/9_advanced/bt-ext-adv-chained/simplicity_sdk_2024.6.1/platform/radio/rail_lib/autogen/librail_release/librail_config_bgm220pc22hna_gcc.a /Users/mafaneh/ble-mastery-training/silabs/9_advanced/bt-ext-adv-chained/simplicity_sdk_2024.6.1/platform/radio/rail_lib/autogen/librail_release/librail_module_efr32xg22_gcc_release.a
-
-LIBS := -lgcc -lc -lm -lnosys
-
